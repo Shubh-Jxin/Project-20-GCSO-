b63620f12f5381cbaa92a6ff11a7fcaa657182c1
shubhjain08 <65031443+shubhjain08@users.noreply.github.com>
2020-05-24 10:38:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
+//creating the variables golbally
 var car, wall;
 var speed, weight;
 
 function setup() {
+  //canvas
   createCanvas(1600,400);
+  
+  //assiging values to the variables
   car= createSprite(50,200,50,50); 
   speed=random(55,90);
   weight=random(400,1500);
@@ -12,10 +16,16 @@ function setup() {
 }
 
 function draw() {
-  background(0); 
+  //background
+  background(0);
+  
+  //collision detection
   if(wall.x - car.x < (car.width + wall.width )/2){
+    //making the car stop after collision
     car.velocityX=0;
+    //to calculate the deformation
     var deformation=0.5*weight*speed*speed/22500;
+    //to change the color of the car after deformation
     if(deformation>180){
       car.shapeColor=color(255, 0, 0);
     }  
@@ -26,5 +36,6 @@ function draw() {
       car.shapeColor=color(0, 255, 0);
     }
   } 
+  //display
   drawSprites();
 }
